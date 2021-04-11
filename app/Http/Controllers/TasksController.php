ee55ee151d5bb1b5e139b69f4f0a52da7fca83bc
sergey <you@example.com>
2021-04-11 16:11:43 +0400

    add comments

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,26 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Models\Task;
+use Illuminate\Routing\Redirector;
+use Illuminate\Validation\ValidationException;
 
+/**
+ * Class TasksController
+ *
+ * @package App\Http\Controllers
+ */
 class TasksController extends Controller
 {
+    /**
+     * @return Application|Factory|View
+     */
     public function index()
     {
         $tasks = auth()->user()->tasks();
         return view('dashboard', compact('tasks'));
     }
+
+    /**
+     * @return Application|Factory|View
+     */
     public function add()
     {
         return view('add');
     }
 
+    /**
+     * @param Request $request
+     * @return Application|RedirectResponse|Redirector
+     * @throws ValidationException
+     */
     public function create(Request $request)
     {
-        $this->validate($request, [
-            'description' => 'required'
-        ]);
+        $this->validate($request, ['description' => 'required']);
         $task = new Task();
         $task->description = $request->description;
         $task->user_id = auth()->user()->id;
@@ -29,29 +50,32 @@ class TasksController extends Controller
         return redirect('/dashboard');
     }
 
+    /**
+     * @param Task $task
+     * @return Application|Factory|View|RedirectResponse|Redirector
+     */
     public function edit(Task $task)
     {
-
-        if (auth()->user()->id == $task->user_id)
-        {
+        if (auth()->user()->id == $task->user_id) {
             return view('edit', compact('task'));
-        }
-        else {
+        } else {
             return redirect('/dashboard');
         }
     }
 
+    /**
+     * @param Request $request
+     * @param Task $task
+     * @return Application|RedirectResponse|Redirector
+     * @throws ValidationException
+     */
     public function update(Request $request, Task $task)
     {
-        if(isset($_POST['delete'])) {
+        if (isset($_POST['delete'])) {
             $task->delete();
             return redirect('/dashboard');
-        }
-        else
-        {
-            $this->validate($request, [
-                'description' => 'required'
-            ]);
+        } else {
+            $this->validate($request, ['description' => 'required']);
             $task->description = $request->description;
             $task->save();
             return redirect('/dashboard');
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,12 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * Class Task
+ *
+ * @package App\Models
+ */
 class Task extends Model
 {
     use HasFactory;
 
+    /**
+     * @return BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
